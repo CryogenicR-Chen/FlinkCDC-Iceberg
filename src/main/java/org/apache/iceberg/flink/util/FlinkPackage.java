210aa7e53905a81d37174fa752445aeb6f844128
chenjianghan <2637212633@qq.com>
2023-12-15 11:31:36 +0800

    update2

---
+++ src/main/java/org/apache/iceberg/flink/util/FlinkPackage.java
@@ -0,0 +1,12 @@
+package org.apache.iceberg.flink.util;
+
+public class FlinkPackage {
+    private static final String VERSION = "1.15.4";
+
+    private FlinkPackage() {
+    }
+
+    public static String version() {
+        return "1.15.4";
+    }
+}
